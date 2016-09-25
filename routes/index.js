683858cb2c539ba6afb836d7ec73a958bc4fa477
tsteer <tasminsteer@hotmail.com>
2016-09-25 19:40:07 +0100

    Put_Post_Get

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,82 @@
 var express = require('express');
 var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('mydb.db');
+var db = new sqlite3.Database('mydbtest.db');
 var bodyParser = require('body-parser');
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: true })
 
-db.run("CREATE TABLE IF NOT EXISTS person (first_name TEXT, last_name TEXT)");
+db.run("CREATE TABLE IF NOT EXISTS person (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT)");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.all("SELECT * FROM person", function(err, rows) {
+    var people = "";
+    rows.forEach(function(row) {
+      console.log("Found a person: " + row.first_name + " " + row.last_name);
+      people += row.first_name + " " + row.last_name + ", ";
+    });
+    res.render('index', {
+      title: 'Express',
+      people: people
+    });
+  });
 });
+/*
+  res.render('index', { title: 'Express',
 
-router.post('/', function(req, res, next) {
+   //    people: [ {name: "David"}, {name: "Tasmin"}]
+   people:  db.run("SELECT * FROM person", function(err, row) {
+     
 
-  response = {
+      row = {
     first_name:req.query.first_name,
     last_name:req.query.last_name
-  };
 
-  var stmt = db.run("INSERT INTO person VALUES (?, ?)", [response.first_name, response.last_name]);
-  
-  console.log(response);
-  res.end(JSON.stringify(response));
+    //first_name:req.body.first_name,
+    //last_name:req.body.last_name
+  };
+      console.log('Correct');  
+     console.log(row);
+      console.log("this" + row);
+
+})
+   }); 
+*/
+
+router.post('/', function(req, res, next){
+    response = {
+    //first_name:req.query.first_name,
+    //last_name:req.query.last_name
+    first_name:req.body.first_name,
+    last_name:req.body.last_name
+  };
+  var id = req.body.id;
+  console.log("test1");
+  db.run("UPDATE Person SET first_name = ? WHERE id = ?", [response.first_name, 1], function(err, result){   
+  });
+  console.log("test2");
+  res.render('index', {
+    title: 'Express' 
+  });
+});   
+/*
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express',
+    people: [ {name: "David"}, {name: "Tasmin"}]
+   });
+}); */
+
+router.post('/', function(req, res, next) {
+  response = {
+    //first_name:req.query.first_name,
+    //last_name:req.query.last_name
+    first_name:req.body.first_name,
+    last_name:req.body.last_name
+  };
+  var stmt = db.run("INSERT INTO person VALUES (NULL, ?, ?)", [response.first_name, response.last_name]);
+  //console.log(response);
+  //res.end(JSON.stringify(response));
 });
 /*
 router.get('/', function(req, res, next) {
