e9cbbdfd44f557a211ce8f659a618e9493876cd5
tsteer <tasminsteer@hotmail.com>
2016-12-31 23:28:39 +0000

    Stats_1_Complete

---
+++ routes/roomstatistics.js
@@ -0,0 +1,51 @@
+module.exports = function(router, db, apiToken, querystring) {
+	
+  router.get('/committee/:id/:club_id/statistics/:team_id/attendancestatistics', function(req, res, next) {
+	  db.all("select * from join_event where team_id = ? ORDER BY event_id ASC", [req.params.team_id], function(err, rows) {
+			event_attendance_list = [];
+			if (err) {
+			  console.log("error:" + err);
+			  res.send("error");
+			  return;
+			}else{
+			  var current_event = 0;
+			  current_event = rows[0].event_id;
+			  var attendance_count = 0;
+			  rows.forEach(function(row){
+				  if(current_event == row.event_id){
+				    if(row.present == 'TRUE'){
+				      attendance_count = attendance_count + 1;
+				    }else{
+						}	
+				    event_attendance_list.push(attendance_count); 
+				  }else{
+				    attendance_count = 0;
+				    current_event = row.event_id;
+				    if(row.present == 'TRUE'){
+				     	attendance_count = attendance_count + 1;
+				    }else{
+	         	}	
+				  }
+			  });	
+				event_date_list = [];
+				var total_event_attend = 0;
+				db.all("select * from event where team_id = ? ORDER BY event_id ASC", [req.params.team_id], function(err, rows) {
+				  if (err) {
+				    console.log("error:" + err);
+			      res.send("error");
+		        return;
+		      }else{
+					 	rows.forEach(function(row){
+				     	event_date_list.push(row.event_date);
+			      });
+		      	db.all("select * from join_team where team_id = ? ", [req.params.team_id], function(err, rows) {
+							var team_size = 0;
+							team_size = rows.length;
+						res.render('attendancestatistics', {id:req.params.id, club_id:req.params.club_id, team_id:req.params.team_id, team_size: team_size, total_event_attend: total_event_attend, event_date_list: event_date_list, event_attendance_list: event_attendance_list});
+					 	});
+					 }
+				});
+			}
+		});
+	});    	
+};
--- routes/viewregister.js
+++ routes/viewregister.js
@@ -1,64 +1,57 @@
 module.exports = function(router, db, apiToken, querystring) {
   router.get('/committee/:id/:club_id/statistics/:team_id/viewregister', function(req, res, next) {
- // 	var register = [];
-    var event = [];
-    var member = {};
-    db.all("select * from join_event where team_id = ? ORDER BY event_id ASC", [req.params.team_id], function(err, rows) {
+    var team = [];
+    var member = [];
+    db.all("select * from join_team WHERE team_id = ? ORDER BY holder_id ASC", [req.params.team_id], function(err, rows) {
       if (err) {
         console.log("error:" + err);
         res.send("error");
         return;
-      } else{
-          var event_tracker = rows[0].event_id;
-          var i = 0;
-          event[i] = [];
-      	rows.forEach(function(row){
-        	if(event_tracker == row.event_id){
-          }else{
-            i++;
-            event[i] = [];
-    //        console.log("number " + i);
-            event_tracker = row.event_id;
-          }
-        });
-    		db.all("select * from join_event left join event on join_event.event_id = event.event_id left join person on person.id = join_event.holder_id where join_event.team_id = ? ORDER BY event_id ASC, person.id ASC", [req.params.team_id], function(err, rows) {
-  	      if (err) {
-  	        console.log("error:" + err);
-  	        res.send("error");
-  	        return;
-  	      } else {
-            var event_tracker_second = rows[0].event_id;
-            var j = 0;
-        		rows.forEach(function(row){
-              if(event_tracker_second == row.event_id){
-            		member = {event_id: row.event_id, holder_id: row.holder_id, present: row.present, first_name: row.first_name, last_name: row.last_name};
-            		event[j].push(member);
-      //      		console.log("asdfsadfs " + j + JSON.stringify(event[j]));
-              }else{
-            //    register.push(event[j]);
-                j++;
-                member = {event_id: row.event_id, holder_id: row.holder_id, present: row.present, first_name: row.first_name, last_name: row.last_name};
-                event[j].push(member);
-                event_tracker_second = row.event_id;
-              }
-            });
-              //  register.push(event[j]);
-        //         console.log("this one " + JSON.stringify(register));
-         //   console.log("lenth " + event.length);
-          /*  for (var k = 0; k < event.length; k++) {
-               console.log("blah " + k + JSON.stringify(event[k]));
-            }*/
-          //    console.log(JSON.stringify(event));
-console.log("testing this: " + event);
-       //   console.log("testing this: " + JSON.stringify(event[0].member));
+      }else{
+        rows.forEach(function(row){
+          team.push(row.holder_id);
+        });  
+        for(i = 1; i < team.length + 1; i++){ 
+          
 
-console.log("hhhhhhhh: " + event.property);
+          member[i] = [];
+          var temp_holder_id = team[i];
+          db.all("select * from join_event WHERE team_id = ? AND holder_id = ?", [req.params.team_id, temp_holder_id], function(err, rows) {
+            rows.forEach(function(row){
+            //   console.log( "1" + JSON.stringify(row));
+             member.push(row.present);
+              console.log( "1" + i + JSON.stringify(member));
+            });  
+                 console.log(  "2" + i + JSON.stringify(member));  
+          }); 
+            console.log( "3" +  i + JSON.stringify(member));
+        };
+      };  
+    //   console.log("2" + JSON.stringify(team));
+      //  console.log("3" + JSON.stringify(attendance));
+        //   console.log("4" + attendance);
+        res.render('viewregister', {id: req.params.id, club_id: req.params.club_id, team_id: req.params.team_id});
+    
+    }); 
 
-        res.render('viewregister', {id: req.params.id, club_id: req.params.club_id, team_id: req.params.team_id, event: event});
-  
-          };
-       });
-      };
-    });
-  });		
+  });  
 };  
+
+/*
+    var event = [];
+    var member = {};
+    db.all("select * from join_event left join event on join_event.event_id = event.event_id left join person on person.id = join_event.holder_id where join_event.team_id = ? ORDER BY event_id ASC, person.id ASC", [req.params.team_id], function(err, rows) {
+      if (err) {
+        console.log("error:" + err);
+        res.send("error");
+        return;
+      }else {
+        rows.forEach(function(row){
+          member = {person_id: row.id, event_date: row.event_date, first_name: row.first_name, last_name: row.last_name, present: row.present};
+          event.push(member);
+        });
+        console.log(JSON.stringify(event));
+        res.render('viewregister', {id: req.params.id, club_id: req.params.club_id, team_id: req.params.team_id, event: event});
+      }  
+    });
+  });  */
